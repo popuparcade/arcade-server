639ef0bbd9c59676f46f0183d81a5ce45af76abe
sethvincent <sethvincent@gmail.com>
2015-09-18 22:10:27 -0700

    send gifs to server

--- browser.js
+++ browser.js
@@ -1,14 +1,31 @@
+var request = require('xhr')
 var socketIO = require('socket.io-client')
 
 module.exports = function controls (options) {
   options = options || {}
-  var socket = socketIO(options.host || 'http://localhost:4444')
+  var host = options.host || 'http://localhost:4444'
+  var socket = socketIO(host)
 
-  socket.on('connect', function () {
-    console.log(socket.id)
-  })
-
+  socket.on('connect', function () {})
   socket.on('disconnect', function () {})
+
+  socket.sendGif = function sendGif (method, path, params, cb) {
+    var url = host + '/gif'
+    var body = new FormData()
+
+    params.files.forEach(function (file, index) {
+      body.append(index.toString, file)
+    })
+
+    request({
+      method: method || 'POST',
+      url: url,
+      body: body
+    }, function (err, response, body) {
+      if (response.statusCode >= 400) return cb({ error: { status: response.statusCode } })
+      return cb(null, response, JSON.parse(body))
+    })
+  }
 
   return socket
 }
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+var fs = require('fs')
+var path = require('path')
+var Busboy = require('busboy')
+var st = require('st')
+var serveStaticFiles = st({ path: __dirname + '/static', url: '/static' })
+
 var app = module.exports = require('http').createServer(function handler (req, res) {
-  res.writeHead(200)
-  res.end('hi')
+  if (serveStaticFiles(req, res)) return
+  if (req.method === 'POST' && req.url === '/gif') {
+    receiveGif(req, res)
+  } else {
+    res.writeHead(200)
+    res.end('hi')
+  }
 }, { serveClient: false })
 
 if (require.main === module) {
@@ -17,3 +28,28 @@ io.on('connection', function (socket) {
     socket.broadcast.emit('move', player, position)
   })
 })
+
+function receiveGif (req, res) {
+  var busboy = new Busboy({ headers: req.headers })
+
+  busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+    console.log(file, filename, encoding)
+    var filepath = path.join(__dirname, 'static', filename)
+    var stream = fs.createWriteStream(filepath, {
+      flags: 'w'
+    })
+
+    file.on('data', function (data) {
+      stream.write(data)
+    })
+
+    file.on('end', function () {
+      stream.end()
+    })
+  })
+
+  busboy.on('finish', function () {
+    res.writeHead('200')
+    res.end()
+  })
+}
