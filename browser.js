94ec04fe1f665dcf8eaf76f185f4aa86272ae875
sethvincent <sethvincent@gmail.com>
2015-09-19 11:37:28 -0700

    new ip

--- browser.js
+++ browser.js
@@ -2,7 +2,7 @@ var socketIO = require('socket.io-client')
 
 module.exports = function controls (options) {
   options = options || {}
-  var host = options.host || 'http://localhost:4444'
+  var host = options.host || 'http://192.168.43.13:4444'
   var socket = socketIO(host)
 
   socket.on('disconnect', function () {})
